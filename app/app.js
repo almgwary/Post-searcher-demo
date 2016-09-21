df7d74f17eb308fb5f8566443974f43c86089128
almgwary <almgwaryfci@gmail.com>
2016-09-21 19:22:39 +0200

    Revert "add agGrid"
    This reverts commit 832dbebd24d02a5523d5fba0af5a37a42a1b4095.

--- app/app.js
+++ app/app.js
@@ -1,10 +1,8 @@
-agGrid.initialiseAgGridWithAngular1(angular);
 'use strict';
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
-  'agGrid',
   'myApp.view1',
   'myApp.view2',
   'myApp.version'
