46adf149ec842b7aed5d37177a7c2a245364b2ba
almgwary <almgwaryfci@gmail.com>
2016-09-25 19:32:45 +0200

    clean post service

--- app/service/post/post.service.js
+++ app/service/post/post.service.js
@@ -1,12 +1,16 @@
-angular.module('myApp')
-  // Service
-  .service("postsService", ['$q', '$http', function ($q, $http) {
-   
-    this.getPosts = function (query) {
+(function () {
+  angular.module('myApp')
+  .service("postsService", postsService);
+  postsService.$inject= ['$q', '$http'];
+  function postsService($q, $http) {
+
+    this.getPosts = getPosts;
+    function getPosts(query) {
       return $http({
-          url:'https://jsonplaceholder.typicode.com/posts', 
-          method:"GET",
-          params:query
+        url: 'https://jsonplaceholder.typicode.com/posts',
+        method: "GET",
+        params: query
       });
     };
-  }])
+  }
+})();
